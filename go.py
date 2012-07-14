301dd1812a413d49ff5232fbef80df9d0c586d1c
Samat K Jain <samat@samat.org>
2012-07-13 22:55:57 -0600

    Refactor into a function

--- go.py
+++ go.py
@@ -9,38 +9,52 @@ from lxml import etree
 headers = {'user-agent': 'Code 66 hackathon'}
 namespaces = {'kml': 'http://www.opengis.net/kml/2.2'}
 
-session = requests.session(headers=headers)
+def go():
+    session = requests.session(headers=headers)
 
-d = session.get('http://data.cabq.gov/transit/realtime/introute/intallbuses.kml')
-raw_document = d.content
-print 'raw document length:', len(raw_document)
+    d = session.get('http://data.cabq.gov/transit/realtime/introute/intallbuses.kml')
+    raw_document = d.content
+    print 'raw document length:', len(raw_document)
 
-raw_document = file('tests/intallbuses.kml').read()
+    raw_document = file('tests/intallbuses.kml').read()
 
-try:
-    raw_document = raw_document.encode('utf-8')
-except UnicodeDecodeError:
-    pass
+    try:
+        raw_document = raw_document.encode('utf-8')
+    except UnicodeDecodeError:
+        pass
 
-t = etree.fromstring(raw_document)
+    t = etree.fromstring(raw_document)
 
-bus_elements = t.xpath('//kml:Placemark', namespaces=namespaces)
-for bus_element in bus_elements:
-    r = {}
-    bus_id = bus_element.xpath('kml:name', namespaces=namespaces)[0].text
-    route_id = bus_element.xpath('kml:description/kml:table/kml:tr/kml:td[text()="Route"]/following-sibling::*', namespaces=namespaces)
-    if not route_id:
-        continue
-    route_id = route_id[0].text
-    if route_id == 'Off Duty':
-        continue
-    r['route_id'] = route_id
+    bus_elements = t.xpath('//kml:Placemark', namespaces=namespaces)
+    bus_elements_output = []
+    for bus_element in bus_elements:
+        r = {}
 
-    speed = bus_element.xpath('kml:description/kml:table/kml:tr/kml:td[text()="Speed"]/following-sibling::*', namespaces=namespaces)[0].text
+        # Bus ID
+        bus_id = bus_element.xpath('kml:name', namespaces=namespaces)[0].text
 
-    msg_time = bus_element.xpath('kml:description/kml:table/kml:tr/kml:td[text()="Msg Time"]/following-sibling::*', namespaces=namespaces)[0].text
-    r['msg_time'] = msg_time
+        # Route ID
+        route_id = bus_element.xpath('kml:description/kml:table/kml:tr/kml:td[text()="Route"]/following-sibling::*', namespaces=namespaces)
+        if not route_id:
+            continue
+        route_id = route_id[0].text
+        if route_id == 'Off Duty':
+            continue
+        r['route_id'] = route_id
 
-    r['bus_id'] = bus_id
-    r['speed'] = speed
-    print r
+        # Speed
+        speed = bus_element.xpath('kml:description/kml:table/kml:tr/kml:td[text()="Speed"]/following-sibling::*', namespaces=namespaces)[0].text
+        speed = speed.split()[0]
+        r['speed'] = speed
+
+        msg_time = bus_element.xpath('kml:description/kml:table/kml:tr/kml:td[text()="Msg Time"]/following-sibling::*', namespaces=namespaces)[0].text
+        r['msg_time'] = msg_time
+
+        r['bus_id'] = bus_id
+        r['speed'] = speed
+        bus_elements_output.append(r)
+    return bus_elements_output
+
+if __name__ == '__main__':
+    import pprint
+    pprint.pprint(go())
