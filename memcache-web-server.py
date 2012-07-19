fd2292b5058cdd1c2addc1608e587bb053327abe
Samat K Jain <samat@samat.org>
2012-07-19 15:12:24 -0600

    Access memcache server via a global, so connection is only made once

--- memcache-web-server.py
+++ memcache-web-server.py
@@ -4,10 +4,10 @@ import memcache
 
 from bottle import response, route
 
+mc = memcache.Client(['127.0.0.1:11211'], debug=0)
+
 @route('/')
 def get():
-    mc = memcache.Client(['127.0.0.1:11211'], debug=0)
-
     response.content_type = 'application/json; charset=utf-8'
     response.set_header('Access-Control-Allow-Origin', '*')
     return mc.get('latest')
